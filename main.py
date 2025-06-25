218d18f740de112480063daad77324f5faf7b770
dan01001 <dmajewski@gmail.com>
2025-06-24 22:44:37 -0400

    Game logic code update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 
@@ -11,13 +12,25 @@ def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    playr = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        dt = clock.tick(60) / 1000.0   
+
         pygame.Surface.fill(screen,(0,0,0))
+        playr.draw(screen)
         pygame.display.flip()
+        
+        
 
 
 
